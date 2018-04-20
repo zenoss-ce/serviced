88b9e0265a183523154d01aa30f61ca21142ecae
Mike Orr <morr@zenoss.com>
2018-04-20 17:22:58 -0500

    getting closer - need to tweak inject items

--- web/ui/src/common/auth0Service.js
+++ web/ui/src/common/auth0Service.js
@@ -22,7 +22,6 @@
                 };
 
                 function login() {
-                    debugger
                     angularAuth0.authorize();
                 }
                 //
@@ -92,5 +91,48 @@
                     logout: logout,
                     checkLogin: checkLogin
                 };
-            }]);
+            }])
+        .config(config);
+
+    config.$inject = [
+        '$stateProvider',
+        '$locationProvider',
+        '$urlRouterProvider',
+        'angularAuth0Provider'
+    ];
+
+    function config($stateProvider, $locationProvider, $urlRouterProvider,angularAuth0Provider) {
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                controller: 'HomeController',
+                templateUrl: 'app/home/home.html',
+                controllerAs: 'vm'
+            })
+            .state('callback', {
+                url: '/callback',
+                controller: 'CallbackController',
+                templateUrl: 'app/callback/callback.html',
+                controllerAs: 'vm'
+            });
+
+        // Initialization for the angular-auth0 library
+        angularAuth0Provider.init({
+            clientID: 'xQF6jCIx6ZynvlvzT8ZWWrbOswcgCwH9',
+            domain: 'zenoss-dev.auth0.com',
+            responseType: 'token id_token',
+            audience: 'https://zenoss-dev.auth0.com/userinfo',
+            redirectUri: 'http://localhost:3000/callback',
+            scope: 'openid'
+        });
+
+        $urlRouterProvider.otherwise('/');
+
+        $locationProvider.hashPrefix('');
+
+        /// Comment out the line below to run the app
+        // without HTML5 mode (will use hashes in routes)
+        $locationProvider.html5Mode(true);
+    }
 })();
